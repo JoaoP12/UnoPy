065d2d9a36b231b43e5be10560b3bf43737d4519
Joao Pedro <jplopes.git@gmail.com>
2020-07-09 12:59:38 -0400

    Implemented the specialCard class with its methods to get and return cards' points

--- card.py
+++ card.py
@@ -26,7 +26,40 @@ class normalCard(Card):
         return self.card_points
 
 class specialCard(Card):
+    '''
+    Stores the special cards of the game, those ones that have a special function
+    such as reverse, draw 2 cards and, draw 4 cards etc.
+    The constructor takes the card's name and color as the normalCard class, but 
+    instead of a number, it takes the card's special function that will generate
+    a different effect in the game.
+    '''
     
-    def __init__(self):
-        pass
-        
+    def __init__(self, color, name, function):
+        super().__init__(color, name)
+        self.effect_in_game = function
+    
+    def get_special_cards_name(self):
+        '''
+        Returns a dictionary with the names of all the special cards' names and
+        the points according to each one.
+        '''
+        names = {
+            'Reverse': 20,
+            'Skip': 20,
+            'Draw two': 20,
+            'Wild': 50,
+            'Wild draw four': 50
+        }
+        return names
+    
+    def get_card_points(self):
+        '''
+        Filters the name of the card according to the self.name instance variable.
+        After comparing it with the names of the special cards got from the self.get
+        _special_cards_name method.
+        Then it returns how many points does the card values according to its name.
+        '''
+        special_cards_name = self.get_special_cards_name()
+        for name in special_cards_name.keys():
+            if name == self.card_name:
+                return special_cards_name[name]
