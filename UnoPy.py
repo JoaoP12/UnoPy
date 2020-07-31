9c9c6bf0db707e5e2b45609e0fc99fdb4f4cd2ef
Joao Pedro <janesgbs@gmail.com>
2020-07-31 08:59:10 -0400

    Finished what seems to be the first version

--- UnoPy.py
+++ UnoPy.py
@@ -17,19 +17,38 @@ def menu():
                     number_of_players = int(input("How many players will play today?\n Min: 2 players - Max: 10 players\n"))
                     
                 break
+            
             except ValueError:
                 continue
+            
+    return ask_players_names(number_of_players)
+
+def ask_players_names(number_of_players):
     print("Now type all the players names...")
-    players_names = []
+    players_string = ""
     for player in range(0, number_of_players):
         current_player_name = input(f"\nPlayer {player} name --> ")
-        players_names.append(current_player_name)
-    '''
-    Cadastrar os players com a class Player.
-    Distribuir cartas.
-    '''
+        players_string += current_player_name + "\n"
+        game_session.insert_player(current_player_name)
+    print("Players:\n")
+    print(players_string)
     
+    
+def distribute_cards(players):
+    for player in players:
+        for card_drown in range(7):
+            game_session.draw_card_to_player(player)
     
 
 def main():
-    pass
+    menu()
+    print("Distributing cards...\n")
+    distribute_cards(game_session.players)
+    print("Done!\n")
+    print("Game Starting...\n")
+    game_session.start_game()
+    
+
+deck = GenerateCards().get_cards()
+game_session = Game(deck)
+main()
